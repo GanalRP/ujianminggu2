01030ce4c4d85cf0859f413923f792bdf8069b4e
GanalRP <gregoriusganal@gmail.com>
2025-03-15 19:20:08 +0700

    Update Main.java

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,17 +1,33 @@
 package com.juaracoding;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ATM atm = new ATM(1000.0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Initial balance: " + atm.lihatSaldo());
+
+        atm.setorUang(500.0);
+        System.out.println("Balance after deposit: " + atm.lihatSaldo());
+
+        atm.tarikUang(200.0);
+        System.out.println("Balance after withdrawal: " + atm.lihatSaldo());
+
+        try {
+            atm.setorUang(-100.0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception on deposit: " + e.getMessage());
+        }
+
+        try {
+            atm.tarikUang(2000.0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception on withdrawal: " + e.getMessage());
+        }
+
+        try {
+            atm.tarikUang(-100.0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception on withdrawal: " + e.getMessage());
         }
     }
 }
